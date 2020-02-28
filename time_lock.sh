9f2b0e4864796e78df31dd7f036e931a1eec7bb3
Bruno de Jesus Silva <bruno.silva@etaure.srv>
2020-02-28 13:07:47 -0300

    Correção dos comentários

--- time_lock.sh
+++ time_lock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script de configuração do sistema
-# Configuração geral para papel de parede
+# Configuração geral para tempo do bloqueio de tela
 
 # Variaveis
+#valor de time é o tempo para o bloqueio da tela em segundos 
 time=600
 dconf_db_usr=user
 dconf_db_sys=local
@@ -21,16 +22,16 @@ mkdir -p /etc/dconf/db/$dconf_db_usr.d
 # Cria pasta para armazenamento de arquivos de trava do dconf
 mkdir -p /etc/dconf/db/$dconf_db_sys.d/locks
 
-# Remove o arquivo de trava da chave do usuário com a configuração do wallpaper
+# Remove o arquivo anterior de trava da chave do usuário com a configuração
 rm /etc/dconf/db/$dconf_db_sys.d/locks/time_lock
 
-# Grava um arquivo de chave do usuário com a configuração do wallpaper
+# Grava um arquivo de chave do usuário dconf
 echo -e "[org/gnome/desktop/session]\nidle-delay $time" > /etc/dconf/db/$dconf_db_sys.d/time_lock.key
 
 # Atualiza o dconf aplicando essa configuração no banco de dados
 dconf update
 
-# Grava o arquivo de trava da chave do usuário com a configuração do wallpaper
+# Grava o arquivo de trava da chave do usuário com a configuração
 echo -e "/org/gnome/desktop/session/idle-delay" > /etc/dconf/db/$dconf_db_sys.d/locks/time_lock
 
 # Atualiza o dconf aplicando essa configuração no banco de dados
