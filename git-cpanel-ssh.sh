f79ceef0a51ab84e7d948ce7192fb99f6dceac2a
Bruno de Jesus Silva <bruno.silva@etaure.srv>
2020-04-09 08:44:27 -0300

    Incluído script de integração de git e cpanel

---
+++ git-cpanel-ssh.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Informe o nome do usuário local"
+read user
+
+echo "Informe o nome de host do computador local"
+read host
+
+echo "Informe o nome do reposítorio"
+read host
+
+ssh-keygen -t rsa -b 4096 -C "$user@$host"
+
+echo -e "\nCopie a chave abaixo e instale no seu reposítorio privado:"
+
+cat ~/.ssh/id_rsa.pub
+
+echo -e "\nPressione ENTER para continuar"
+
+read
+
+ssh -T git@bitbucket.org
+
+echo "Informe o nome do reposítorio que aparece na saída acima:"
+read repoaux
+
+git clone git@bitbucket.org:$repoaux
+
+rm -rf $repo
